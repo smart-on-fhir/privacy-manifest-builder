97cc3485a6131bac2d4641c0e7e04154f123e0bd
Jake Lambert <jake.lambert@childrens.harvard.edu>
2020-12-21 16:43:18 -0500

    Initial rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
 import questionnaire from "./questionnaire.json";
+import { useReducer } from 'react'
+
+const ANSWER = 'ANSWER'
+
+const initialState = {
+  ready: false,
+  answers: questionnaire.item.reduce((prev, e) => {
+    prev[e.linkId] = null;
+    return prev
+  }, {})
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case ANSWER:
+      const answers = {
+          ...state.answers,
+          [action.linkId]: action.value
+      }
+      return {
+        ...state,
+        answers,
+        ready: Object.values(answers).every(i => !!i)
+      }
+    default:
+        return state
+  }
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,20 +50,27 @@ function App() {
         <br/>
         <br/>
         <div className="row">
-        { questionnaire.item.map((x, i) => (
-            <div className="col-lg-6" key={i}>
-              <div className="card">
-                <div className="card-header"><b>{ x.text }</b></div>
-                <div className="card-body">TODO: Render question UI here...</div>
+        { questionnaire.item.map((item) => (
+          <div key={item.linkId} className="col-lg-6">
+            <div className="card">
+              <div className="card-header"><b>{ item.text }</b></div>
+              <div className="card-body">
+                <QuestionItem
+                  linkId={item.linkId}
+                  type={item.type}
+                  answerOptions={getOptions(item.answerValueSet)}
+                  currentAnswer={state.answers[item.linkId]}
+                  dispatcher={(linkId, value) => void dispatch({ type: ANSWER, linkId, value})}
+                  />
               </div>
-              <br/>
             </div>
-          
+            <br/>
+        </div>
         ))}
         </div>
         <hr/>
         <div className="text-center">
-          <button className="btn btn-primary btn-lg">Generate Privacy Manifest</button>
+          <a className={`btn btn-primary btn-lg ${!state.ready ? 'disabled' : ''}`} href={"data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(state.answers, null, 2))} download="PrivacyManifest.json">Generate Privacy Manifest</a>
         </div>
         <br/>
         <br/>
@@ -47,6 +85,57 @@ function App() {
       </nav>
     </div>
   );
+}
+
+function getOptions(answerValueSet) {
+  if(answerValueSet) {
+    return questionnaire.contained.find(e => `#${e.id}` === answerValueSet).compose.include[0].concept
+  }
+  return null
+}
+
+function OptionsList({ linkId, answerOptions, currentAnswer, dispatcher }) {
+  return answerOptions.map(concept => (
+    <div key={`${linkId}_${concept.code}`} className="form-check">
+    <input className="form-check-input"
+      type="radio" name={linkId}
+      id={`${linkId}_${concept.code}`}
+      value={concept.code}
+      checked={currentAnswer === concept.code}
+      onChange={() => dispatcher(concept.code)}
+      />
+    <label className="form-check-label" htmlFor={`${linkId}_${concept.code}`}>
+      {concept.display}
+    </label>
+  </div>
+  ))
+}
+
+// TODO: Input validation
+function URLInput({ linkId, currentAnswer, dispatcher}) {
+  return <input className="form-control" type="url" name={linkId} id={linkId} placeholder="https://example.com"
+              pattern="https?://.*" size="30"
+              required value={currentAnswer || ''} onChange={e => dispatcher(e.target.value)}/>
+}
+
+function QuestionItem({ type, linkId, answerOptions, currentAnswer, dispatcher}) {
+  switch(type) {
+    case 'choice':
+      return  <OptionsList
+                answerOptions={answerOptions}
+                linkId={linkId}
+                currentAnswer={currentAnswer}
+                dispatcher={(value) => void dispatcher(linkId, value)}
+              />
+    case 'url':
+      return <URLInput
+                linkId={linkId}
+                currentAnswer={currentAnswer}
+                dispatcher={(value) => void dispatcher(linkId, value)}
+              />
+    default:
+      return <div className="error">Error: Unsupported Question Type</div>
+  }
 }
 
 export default App;
